d7d77d428bd55e3e944ad026e480acc075f22922
Tia Peruzzi <tiamariaperuzzi@gmail.com>
2020-10-07 20:14:24 -0400

    HM-70, Task HM-75
    install material design package and add md-toolbar component to playaround with

---
+++ vue-hm-sandbox/landing/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Landing></Landing>
+  </div>
+</template>
+
+<script>
+import Landing from './components/Landing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Landing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ vue-hm-sandbox/landing/src/components/Landing.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <md-toolbar class="md-primary" md-elevation="1">
+      <h3 class="md-title" style="flex: 1">The Halo Mod</h3>
+      <md-button>About</md-button>
+      <md-button class="md-primary">Calculator</md-button>
+    </md-toolbar>
+    </div>
+</template>
+
+<style scoped>
+.md-toolbar {
+    margin-top: 0;
+}
+</style>
+
+<script>
+export default {
+    name: 'Landing'
+}
+</script>
